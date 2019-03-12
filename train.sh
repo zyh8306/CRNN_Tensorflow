f184b763b31ffafa83ae0926abd32e2b3b01c9ed
piginzoo <piginzoo@gmail.com>
2019-03-12 10:47:48 +0800

    add gpu limit

--- train.sh
+++ train.sh
@@ -3,7 +3,8 @@
 #    parser.add_argument('-w', '--weights_path', type=str, help='Path to pre-trained weights.')
 #    parser.add_argument('-j', '--num_threads', type=int, default=int(os.cpu_count()/2),
 #                        help='Number of threads to use in batch shuffling')
-
+# 第一句表明使用第0个GPU，CRNN用第0个GPU，CTPN用第1个GPU，恩，我这么分配的
+CUDA_VISIBLE_DEVICES=0 \
 python \
     -m tools.train_shadownet \
     --dataset_dir=data \
