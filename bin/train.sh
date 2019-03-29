3f99d6921126eb99c8ed56316acf2d1a47e59f18
piginzoo <piginzoo@gmail.com>
2019-03-29 16:14:36 +0800

    bugfix

--- bin/train.sh
+++ bin/train.sh
@@ -1,3 +1,9 @@
+if [ "$1" = "stop" ]; then
+    echo "停止训练"
+    kill -9 `ps aux|grep shadow| grep -v grep|awk '{print $2}'`
+    exit
+fi
+
 # 第一句表明使用第0个GPU，CRNN用第0个GPU，CTPN用第1个GPU，恩，我这么分配的
 CUDA_VISIBLE_DEVICES=0 \
 python \
