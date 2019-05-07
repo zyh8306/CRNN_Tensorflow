252c2e3a7ddd8491fa4c4e8f242237bb05e714ab
zyh8306 <zyh8306@163.com>
2019-05-07 19:50:45 +0800

    change GPU to 1

--- bin/train.sh
+++ bin/train.sh
@@ -7,7 +7,7 @@ fi
 if [ "$1" = "console" ]; then
     echo "调试模式"
     # 第一句表明使用第0个GPU，CRNN用第0个GPU，CTPN用第1个GPU，恩，我这么分配的
-    CUDA_VISIBLE_DEVICES=0 \
+    CUDA_VISIBLE_DEVICES=1 \
     python \
         -m tools.train \
         --train_dir=data/train \
@@ -22,7 +22,7 @@ if [ "$1" = "console" ]; then
 else
     echo "生产模式"
     # 第一句表明使用第0个GPU，CRNN用第0个GPU，CTPN用第1个GPU，恩，我这么分配的
-    CUDA_VISIBLE_DEVICES=0 \
+    CUDA_VISIBLE_DEVICES=1 \
     python \
         -m tools.train \
         --train_dir=data/train \
