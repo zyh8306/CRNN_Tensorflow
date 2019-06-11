1c806de70c0b17844025f93b7afe862a29395483
zyh8306 <zyh8306@163.com>
2019-06-11 16:16:27 +0800

    update sh

--- bin/train.sh
+++ bin/train.sh
@@ -23,17 +23,11 @@ else
     echo "生产模式"
     # 第一句表明使用第0个GPU，CRNN用第0个GPU，CTPN用第1个GPU，恩，我这么分配的
     CUDA_VISIBLE_DEVICES=1 \
-    python \
-        -m tools.newtrain \
-        --train_dir=data/train \
-        --label_file=data/train.txt \
-        --charset=charset6k.txt \
+    nohup python \
+        -m tools.train --train_dir=data/ --charset=charset6k.txt --debug=True \
         --name=crnn \
-        --validate_steps=10000 \
-        --validate_file=data/test.txt \
         --tboard_dir=tboard \
-        --debug=True \
-        >> ./logs/crnn.log 2>&1
+        >> ./logs/stdout.out 2>&1 &
 fi
 #    --weights_path=model/shadownet/shadownet_2019-03-11-15-13-38.ckpt-8 \
 
